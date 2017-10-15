a5a816170f8f703817ed610d12ca0cf2ab949f8d
sebgrebe <sebbecker@gmx.net>
2017-10-15 19:17:52 +0100

    fix index

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 var express = require('express')
 var mongo = require('mongodb').MongoClient
 var isURL = require('is-url')
-var assert = require('assert')
 var app = express()
 var uri = 'mongodb://heroku_hj043f9f:jjqeethl1t8cnn3smfao50kaai@ds119675.mlab.com:19675/heroku_hj043f9f'
-
 var local_db = 'mongodb://localhost:27017/url-shortener'
 var db_url = uri || local_db
 const port = process.env.PORT || 4000;
 
-console.log('App running on Port 4000')
+console.log('App running on '+port)
 
 app.use(express.static('app'))
 
-// app.get('/delete',(req,res) => {
-// 		mongo.connect(db_url, (err, db) => {
-//   	var collection = db.collection('documents');
-//   	collection.remove()
-//   	res.send("deleted")
-//   })
-// })
-
-//handle new urls to shorten
+/* Shorten URLs. Shortened URLs are built from the index of the document the new URL creates 
+   in the database. 
+   Checks whether the URL that was typed in follows the URL format, using the package is-url*/
 app.get('/new/*',(req,res) => {
-  mongo.connect(uri, (err, db) => {
+  mongo.connect(db_url, (err, db) => {
   	var url = req.url.substr(5,req.url.length-1)
   	if (isURL(url)) {
   		var host = req.get('host')
@@ -51,9 +43,10 @@ app.get('/new/*',(req,res) => {
   });
 })
 
-//link to shortened urls
+/*Redirects shortened URLs to the original URL. If someone types in a link that doesn't exist,
+returns that this link doesn't exist*/
 app.get('/*',(req,res) => {
-  mongo.connect(uri, (err, db) => {
+  mongo.connect(db_url, (err, db) => {
   	var shortened = req.url.substr(1,req.url.length-1)
   	shortened = parseInt(shortened)
   	var collection = db.collection('documents');
@@ -64,7 +57,7 @@ app.get('/*',(req,res) => {
 	  		res.redirect(result['url'])
 	  	}
 	  	else {
-	  		res.send("Shortened URL does not exist")
+	  		res.send({'error:': 'Shortened URL does not exist'})
 	  	}
   	})
   	db.close()
@@ -72,5 +65,13 @@ app.get('/*',(req,res) => {
   });
 })
 
+// To delete the database, uncomment this and got to the /delete path
+// app.get('/delete',(req,res) => {
+// 		mongo.connect(db_url, (err, db) => {
+//   	var collection = db.collection('documents');
+//   	collection.remove()
+//   	res.send("deleted")
+//   })
+// })
 
 app.listen(port); 
